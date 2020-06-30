3fccc512240aa2a1b8f882695eacac9c9d23d9a6
CESAR HUGO <cesardelimperio.gmail.com>
2020-06-30 13:09:41 -0300

    Agregue oracion de texto a NuevoVideoGit

--- NuevoVideoGit.py
+++ NuevoVideoGit.py
@@ -1,1 +1,1 @@
-print("Hola a tod@s, en Este Video explicamos el uso de Git y de Giyhub y veremos porque no puede subirlo directamente a GitHub")
+print("Hola a tod@s, en Este Video explicamos el uso de Git y de Giyhub y veremos porque no puede subirlo directamente a GitHub. Mi primer cambio.")
